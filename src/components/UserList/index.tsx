39910a8a45f4f674108a9f94ee135fe3f6c5cebe
rodrigosakamoto <rodosakamoto@gmail.com>
2020-07-03 18:38:39 -0300

    add component UserList

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Container, Role, User, Avatar } from './styles';
+
+interface UserProps  {
+  nickname: string;
+  status: string;
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot, status }) => {
+  return (
+    <User>
+      <Avatar status={status} className={isBot ? 'bot': ''} />
+
+      <strong>{nickname}</strong>
+
+      {isBot && <span>Bot</span>}
+    </User>
+
+  );
+};
+
+
+const UserList: React.FC = () => {
+  return(
+    <Container>
+      <Role>Disponível - 1</Role>
+
+      <UserRow nickname="Rodrigo Sakamoto" status="disponivel" />
+      <UserRow nickname="CaptainMirai" status="ausente" />
+      <UserRow nickname="Mallu" status="ocupado" />
+
+
+      <Role>Offline - 18</Role>
+
+      <UserRow nickname="UG" isBot status="offline"/>
+      <UserRow nickname="Helio" isBot status="offline"/>
+
+    </Container>
+  );
+} 
+
+export default UserList;
---
+++ src/components/UserList/styles.ts
@@ -0,0 +1,134 @@
+import styled, { css } from 'styled-components';
+
+
+interface Props {
+  status?: string;
+}
+
+export const Container = styled.div`
+  grid-area: UL;
+
+  display: flex;
+  flex-direction: column;
+
+
+  padding: 3px 6px 0 16px;
+  background-color: var(--secondary);
+
+  max-height: calc(100vh - 46px);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--tertiary);
+    border-radius: 4px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: var(--secondary);
+  }
+
+`;
+
+export const Role = styled.span`
+  margin-top: 20px;
+
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 500;
+  color: var(--gray);
+`;
+
+export const User = styled.div`
+  margin-top: 5px;
+  padding: 5px;
+
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+  border-radius: 4px;
+  background: transparent;
+  transition: background .2s;
+  &:hover {
+    background: rgba(255, 255, 255, 0.1);
+  }
+
+  > strong {
+    margin-left: 13px;
+    font-weight: 500;
+    color: var(--white);
+    opacity: 0.7;
+
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  > span {
+    margin-left: 9px;
+
+    background-color: var(--discord);
+    color: var(--white);
+    border-radius: 4px;
+    padding: 4px 5px;
+
+    text-transform: uppercase;
+    font-weight: bold;
+    font-weight: 11px;
+  }
+
+`;
+
+export const Avatar = styled.div<Props>`
+  flex-shrink: 0;
+
+  width: 32px;
+  height: 32px;
+
+  background-color: var(--primary);
+  border-radius: 50%;
+  position: relative;
+
+  &::after {
+
+${props => props.status === 'ocupado' && css`
+  background-color: var(--notification);
+`}
+
+${props => props.status === 'offline' && css`
+  background-color: none;
+`}
+
+${props => props.status === 'ausente' && css`
+  background-color: var(--mention-detail);
+`}
+
+${props => props.status === 'disponivel' && css`
+  background-color: #00d166;
+`}
+
+  width: auto;
+  height: 9px;
+
+  padding: 0 4px;
+  
+  position: absolute;
+  bottom: -4px;
+  right: -4px;
+
+  border-radius: 16px;
+  border: 4px solid var(--quaternary);
+  
+  ${props => props.status !== 'offline' && css`
+    content: '';
+  `}
+}
+
+  &.bot {
+    background-color: var(--mention-detail);
+  }
+`;
